41fd1a4181df1b59d7bf71a5a4137a9830d7bb63
Vishal Ankush Gavali <vishalgavali2004@gmail.com>
2025-08-11 01:37:40 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,7 +63,7 @@ export const login = async (req, res) => {
 
         const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
 
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict' }).json({
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'none' }).json({
             _id: user._id,
             username: user.username,
             fullName: user.fullName,
@@ -91,4 +91,5 @@ export const getOtherUsers = async (req, res) => {
     } catch (error) {
         console.log(error);
     }
+
 }
